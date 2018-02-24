90253311f4e3828c176f08bb7981c6aa4536b4c5
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2018-02-24 16:09:31 -0600

    Signup modal added with show/hide functionality. CSS updates to body, comments added to CSS.

--- public/assets/javascript/login-signup.js
+++ public/assets/javascript/login-signup.js
@@ -1,20 +1,40 @@
-// Get the modal
+// login modal variable
 var loginModal = $("#login-modal");
+// signup modal variable
+var signupModal = $("#signup-modal");
 
-// Get the button that opens the modal
+// login button variable
 var loginBtn = $("#login-modal-button");
+// signup button variable
+var signupBtn = $("#signup-modal-button");
 
-// Get the <span> element that closes the modal
+// close login variable
 var loginClose = $("#login-close")
+// close signup variable
+var signupClose = $("#signup-close")
 
+//hide login modal on load
 loginModal.hide();
+//hide signup modal on load
+signupModal.hide();
 
-// When the user clicks on the button, open the modal 
+// when the user clicks on the button, open login modal, close signup modal 
 loginBtn.on("click", function() {
     loginModal.show();
+    signupModal.hide();
+});
+// when the user clicks on the button, open signup modal, close login modal
+signupBtn.on("click", function() {
+    signupModal.show();
+    loginModal.hide();
 });
 
-// When the user clicks on <span> (x), close the modal
+// when the user clicks on <span> (x), close login modal
 loginClose.on("click", function() {
    loginModal.hide();
 });
+// when the user clicks on <span> (x), close signup modal
+signupClose.on("click", function() {
+   signupModal.hide();
+});
+
