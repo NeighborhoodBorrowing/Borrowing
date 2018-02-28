930f91f729c03886f18dc9dd18951da72cb5b298
fuseina <fuseina@gmail.com>
2018-02-27 19:42:58 -0600

    added borrowStatus to memberItem

--- models/borrowedItems.js
+++ models/borrowedItems.js
@@ -1,13 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
   var BorrowedItems = sequelize.define("BorrowedItems",
   {
+    borrowedStatus: {
+      //has this item been borrowed
+      // -1  - request pending, waiting for owner's approval
+      // 0 - request denied
+      // 1 - request approved
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:-1
+    },
     borrowedDate: {
       type: DataTypes.DATE,
-      allowNull: false
+      allowNull: true
     },
     dueDate: {
       type: DataTypes.DATE,
-      allowNull: false
+      allowNull: true
     },
     returnDate: {
       type: DataTypes.DATE,
