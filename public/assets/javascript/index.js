7446f7cc7f45f9bfc18a8adba0c1be50d2ff50b8
Jennifer Williams <jwillakers@hotmail.com>
2018-03-04 21:06:36 -0600

    Add files via upload

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,20 +1,54 @@
-// var animateButton = function(e) {
-//
-//   e.preventDefault;
-//   //reset animation
-//   e.target.classList.remove('animate');
-// 
-//   e.target.classList.add('animate');
-//   setTimeout(function(){
-//     e.target.classList.remove('animate');
-//   },700);
-// };
-//
-// var bubblyButtons = document.getElementsByClassName("bubbly-button");
-//
-// for (var i = 0; i < bubblyButtons.length; i++) {
-//   bubblyButtons[i].addEventListener('click', animateButton, false);
-// }
-// // $('button').click(function(){
-// //    window.location.href='index.html';
-// // })
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
